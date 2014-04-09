0735d7bc167b4f0a9bf42b39b820bd2b5e3b5855
mpsnp <kir_george@mail.ru>
2014-04-09 10:59:18 +0400

    Networks documentation.

--- Include/CGenericNetwork.hxx
+++ Include/CGenericNetwork.hxx
@@ -5,14 +5,41 @@
 
 namespace CNN
 {
+	/**
+	 * @brief Input vector.
+	 * @details Symply typedef just for good looking and code understanding.
+	 */
 	typedef std::vector<float> TInputVector;
+
+	/**
+	 * @brief Output vector.
+	 * @details Symply typedef just for good looking and code understanding.
+	 */
 	typedef std::vector<float> TOutputVector;
 	
+	/**
+	 * @brief Base class for all networks.
+	 */
 	class CGenericNetwork
 	{
 	public:
+		/**
+		 * @brief Default empty constructor.
+		 */
 		CGenericNetwork(){};
+
+		/**
+		 * @brief Default virtual destructor.
+		 */
 		virtual ~CGenericNetwork(){};
+
+		/**
+		 * @brief Processes input vector to output vector.
+		 * @warning Correct processing requires network training.
+		 * 
+		 * @param Input Input vector.
+		 * @return Output vector.
+		 */
 		virtual TOutputVector Calculate(TInputVector Input) = 0;
 	};
 };
--- Include/CPerceptron.hxx
+++ Include/CPerceptron.hxx
@@ -6,29 +6,98 @@
 
 namespace CNN
 {
+	/**
+	 * @brief Vector contains neurons.
+	 * @details Symply typedef just for good looking and code understanding.
+	 */
 	typedef std::vector<CNeuron*> TNeuronLayer;
 	
+	/**
+	 * @brief Perceptron implementation.
+	 * @details Implements multi layer perceptron.
+	 */
 	class CPerceptron : public CGenericNetwork
 	{
 	private:
-		std::vector< std::vector<CNeuron*> > _vLayers;
+		std::vector<TNeuronLayer> _vLayers;
 	public:
+		/**
+		 * @brief Constructs new perceptron.
+		 * @details It only prepares perceptron structure for neurons adding. All layers are blank after construction.
+		 * For neurons adding use AddNeuronToLayer() and FillLayers() methods. 
+		 * @note Don't forget to train it.
+		 * 
+		 * @param HiddenLayers Hidden layers quantity. 
+		 */
 		CPerceptron(int HiddenLayers);
+
+		/**
+		 * @brief Deletes all neurons in it.
+		 */
 		~CPerceptron();
 
 		TOutputVector Calculate(TInputVector Input);
+
+		/**
+		 * @brief Adds specified neuron to specified layer.
+		 * @warning First layer index is 0.
+		 * 
+		 * @param Neuron Neuron instance.
+		 * @param Layer Layer index.
+		 */
 		void AddNeuronToLayer(CNeuron* Neuron, int Layer);
+
+		/**
+		 * @brief Automatically fills layers.
+		 * @details Automatically adds new neurons by calling AddNeuronToLayer(). 
+		 * New neurons are generated with GenerateNewNeuron() method.
+		 * 
+		 * @param NeuronsQuantity Vector contains neurons quantity for each layer. 
+		 * Vector must be the same size as GetLayerCount().
+		 */
 		void FillLayers(std::vector<int> NeuronsQuantity);
 		
+		/**
+		 * @brief Returns layer with specified index.
+		 * 
+		 * @param Layer Layer index.
+		 */
 		TNeuronLayer GetLayer(int Layer);
+
+		/**
+		 * @brief Returns the last layer.
+		 * @details It is simply for convenience purposes.
+		 */
 		TNeuronLayer GetRecieversLayer();
+
+		/**
+		 * @brief Returns first layer.
+		 */
 		TNeuronLayer GetSensorsLayer();
 		
+		/**
+		 * @brief Returns layers count.
+		 */
 		int GetLayerCount();
 
+		/**
+		 * @brief Prints every neuron in layer.
+		 * @todo Add new param --- output stream.
+		 * 
+		 * @param ALayer Layer index.
+		 */
 		void PrintLayer(int ALayer);
+
+		/**
+		 * @brief Prints every layer in network.
+		 */
 		void PrintNetwork();
 		
+		/**
+		 * @brief Generates new neuron.
+		 * @details If you want to use custom neurons, you need override this method.
+		 * @return Pointer to base class CNeuron.
+		 */
 		virtual CNeuron* GenerateNewNeuron();
 	};
 };
